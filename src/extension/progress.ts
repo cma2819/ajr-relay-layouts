0618bb3014bffedb6b630f258a67e5ef4ca3d958
Cma <chicchaima@gmail.com>
2023-09-09 12:56:59 +0900

    fix progress chart

--- src/extension/progress.ts
+++ src/extension/progress.ts
@@ -8,8 +8,27 @@ export const progress = (nodecg: NodeCG): void => {
 
   const games = nodecg.bundleConfig.games;
 
-  const calculateProgress = (results: CheckpointResults): void => {
-    const progress = results.reduce((prev, resultsPerGame, gIndex) => {
+  const totalTimesTo = (results: CheckpointResults, gameIndex: number, segIndex: number, teamIndex: number): number => {
+    return results.reduce((prev, resultsByTeam, gIndex) => {
+      if (gIndex > gameIndex) {
+        return prev;
+      }
+      if (gIndex === gameIndex) {
+        return prev + resultsByTeam.reduce((p, results, sIndex) => {
+          if (sIndex > segIndex) {
+            return p;
+          }
+          return p + (results?.[teamIndex]?.time ?? 0);
+        }, 0);
+      }
+      return prev + resultsByTeam.reduce((p, results) => {
+        return p + (results?.[teamIndex]?.time ?? 0);
+      }, 0);
+    }, 0);
+  }
+
+  const calculateProgress = (cpResults: CheckpointResults): void => {
+    const progress = cpResults.reduce((prev, resultsPerGame, gIndex) => {
       const existsAnyResultInGame = resultsPerGame.some(results => results.some(r => !!r));
       if (!existsAnyResultInGame) {
         return prev;
@@ -18,10 +37,11 @@ export const progress = (nodecg: NodeCG): void => {
         ... prev,
         ... resultsPerGame.map((results, sIndex) => {
           const label = `${games?.[gIndex]?.segments?.[sIndex]}`;
-          const resultTimes = [...results.map(r => r?.time ?? Infinity)].sort((a, b) => {
+          const resultTimes = [...results.map((r, tIndex) => r?.time ? totalTimesTo(cpResults, gIndex, sIndex, tIndex) : Number.MAX_VALUE)].sort((a, b) => {
             return a - b;
           });
-          const ranks = results.map(r => r ? resultTimes.indexOf(r.time) + 1 : null);
+          logger.debug(JSON.stringify(resultTimes));
+          const ranks = results.map((r, tIndex) => r ? resultTimes.indexOf(totalTimesTo(cpResults, gIndex, sIndex, tIndex)) + 1 : null);
 
           return {
             label,
